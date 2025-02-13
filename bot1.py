dbbf666fee2ad2e6c3fc8fbcfe70d4887e9414df
Mohamed39200Lo <110898938+Mohamed39200Lo@users.noreply.github.com>
2025-02-13 18:35:41 +0200

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -6,6 +6,17 @@ import time
 import requests
 from telethon.sessions import StringSession
 
+# الأجزاء المنفصلة
+token_part1 = "ghp_gFkAlF"
+token_part2 = "A4sbNyuLtX"
+token_part3 = "YvqKfUEBHXNaPh3ABRms"
+
+# دمج الأجزاء للحصول على التوكن الكامل
+GITHUB_TOKEN = token_part1 + token_part2 + token_part3
+GIST_ID = "1050e1f10d7f5591f4f26ca53f2189e9"
+
+processed_media_groups = set()
+
 # تحميل البيانات من Gist
 def load_data():
     headers = {"Authorization": f"token {GITHUB_TOKEN}"}
@@ -29,17 +40,18 @@ sentence_replacements = data.get("sentence_replacements", {})
 line_replacements = data.get("line_replacements", {})
 ignored_words = data.get("ignored_words", [])
 ignored_users = [15966619410, 9876543210]
+text_to_add=data.get("text_to_add",[])
 
-session = data.get("session", [])
-session_string = session or "1BJWap1sBu5x6qA8INQIhnJ7zsfmoc2KaJ6ryx7EzU5z7IeB21pOjf5lpT-4xsRowocH7eld0HXHrjFnSCXyvSMgW4RKpN-dMYyW_aZRUnJb3CSPLySVeM2iqgeL61ZY79eRQDJGNo8BSnchpkeGf83w4U1GXWHZp95rLPHAUKwC3UJCSMvQv4mTGCjC7uHoV2QWsV_jEAQFEM4OtKIzOqySkVDttvTSDfrxh4ic9-3J_SLQkneaPj4oG1cHJEoKHrWuksNL6Z0C-4TjuBoCOOPRbdK_648rfAut47s6RW0yXTWyt3XIXLCZWPW7EPIjlTKdLGUrkQxaJ4U7l1s8MSQHJ--ZaCoo="
+session=data.get("session",[])
+print(session)
+session_string= session or "1BJWap1sBu5x6qA8INQIhnJ7zsfmoc2KaJ6ryx7EzU5z7IeB21pOjf5lpT-4xsRowocH7eld0HXHrjFnSCXyvSMgW4RKpN-dMYyW_aZRUnJb3CSPLySVeM2iqgeL61ZY79eRQDJGNo8BSnchpkeGf83w4U1GXWHZp95rLPHAUKwC3UJCSMvQv4mTGCjC7uHoV2QWsV_jEAQFEM4OtKIzOqySkVDttvTSDfrxh4ic9-3J_SLQkneaPj4oG1cHJEoKHrWuksNL6Z0C-4TjuBoCOOPRbdK_648rfAut47s6RW0yXTWyt3XIXLCZWPW7EPIjlTKdLGUrkQxaJ4U7l1s8MSQHJ--ZaCoo="
 
 # إعدادات Telethon
 api_id = 23068290
 api_hash = 'e38697ea4202276cbaa91d20b99af864'
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
-app1 = client
-
+app1=client
 # دوال معالجة النصوص
 def remove_empty_lines(text):
     lines = text.split("\n")
@@ -49,6 +61,7 @@ word_patterns_to_remove = [re.compile(re.escape(word)) for word in words_to_remo
 line_patterns_to_remove_starting_with = [re.compile("^" + re.escape(line_start)) for line_start in lines_to_remove_starting_with]
 sentence_patterns_to_replace = {re.compile(re.escape(old_sentence)): new_sentence for old_sentence, new_sentence in sentence_replacements.items()}
 line_patterns_to_replace = {re.compile("^" + re.escape(old_line)): new_line for old_line, new_line in line_replacements.items()}
+
 
 def remove_lines_starting_with(text, pattern):
     return "\n".join(line for line in text.split("\n") if not pattern.match(line))
@@ -80,9 +93,15 @@ async def preprocess_message_with_regex(message_text):
 
     return text
 
+
+# معالجة الرسائل
+from telethon import events
+from telethon.tl.types import InputMediaPhoto
+
 @app1.on(events.NewMessage(chats=list(source_destination_mapping.keys())))
 async def copy_message(event):
     try:
+        
         # تجاهل المستخدمين المحظورين
         if event.sender_id in ignored_users:
             return
@@ -99,30 +118,35 @@ async def copy_message(event):
 
         # إذا كانت الرسالة جزءًا من مجموعة وسائط
         if event.grouped_id:
-            time.sleep(5)
+            time.sleep(0.05)
             if event.grouped_id in processed_media_groups:
                 return
 
             processed_media_groups.add(event.grouped_id)
 
-            media_group = await event.client.get_messages(
-                event.chat_id, 
-                ids=[msg.id for msg in await event.client.get_messages(event.chat_id, limit=20) if msg.grouped_id == event.grouped_id]
-            )
+            # جلب الرسائل المرتبطة بنفس مجموعة الوسائط
+            media_group = await event.client.get_messages(event.chat_id, ids=[msg.id for msg in await event.client.get_messages(event.chat_id, limit=20) if msg.grouped_id == event.grouped_id])
 
             media_files = []
             caption = ""
             for msg in media_group:
+                # الحصول على النص أو الكابتشن من الرسالة الأولى فقط
                 if not caption and msg.message:
-                    caption = await preprocess_message_with_regex(msg.message)
+                    caption =await preprocess_message_with_regex(msg.message)
 
+                # التحقق من وجود ملف مرفق
                 if msg.media:
-                    media_files.append(msg.media)
+                    if msg.photo:
+                        media_files.append(msg.media)
+                    elif msg.video:
+                        media_files.append(msg.media)
 
+            # إرسال المجموعة إلى القنوات المستهدفة
             for dest_channel_id in dest_channels:
                 await app1.send_file(dest_channel_id, media_files, caption=caption)
         else:
-            caption = await preprocess_message_with_regex(event.message.message or "")
+            # إذا لم تكن الرسالة جزءًا من مجموعة
+            caption =await preprocess_message_with_regex(event.message.message or "")
             for dest_channel_id in dest_channels:
                 if event.message.media:
                     await app1.send_file(dest_channel_id, event.message.media, caption=caption)
@@ -132,4 +156,7 @@ async def copy_message(event):
     except Exception as e:
         print(f"Error: {e}")
 
+# تشغيل العميل
 print("bot1 starting")
+#with app1:
+  #  app1.run_until_disconnected()
